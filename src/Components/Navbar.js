dcf3eef989af5ccae3b8dc1b36c4850a9826c298
Musfiqua <zmushfiqua@gmail.com>
2023-01-24 16:24:35 +0600

    Navbar Complete

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import logo from '../images/logo.png'
 
+/* REACT FONTAWESOME IMPORT */
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
   <div className="container">
   <a className='navbar-brand' href="#"><img className='logo' src={logo} alt="portfolio-logo..." /></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span className="navbar-toggler-icon"></span>
+    <FontAwesomeIcon icon={faBars} style={{color:'#fff'}}/>
   </button>
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav m-auto">
