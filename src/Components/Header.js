be759b20d3a70272132df447b32d101d2df3d22c
Musfiqua <zmushfiqua@gmail.com>
2023-01-25 18:03:49 +0600

    navbar and header under constraction

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import hero from '../images/hero.jpg'
 // import Typed from 'react-typed'
 
 export default function Header() {
   return (
     <div className='header-wraper'>
+      <div className="header-logo">
+        <img className='hero' src={hero} alt="Hero logo" />
+      </div>
         <div className='main-info'>
             <h1>I am a Web developer</h1>
-            {/* <Typed className='typed-text'
-            strings={["react developer, App developer, facebook ads"]}
-            /> */}
             <a href="#" className='contact-button'>Contact</a>
         </div>
     </div>
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import logo from '../images/logo.jpg'
+// import logo from '../images/logo.jpg'
+import navbar from '../images/navbar.jpg'
 
 /* REACT FONTAWESOME IMPORT */
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
@@ -8,7 +9,7 @@ export default function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
   <div className="container">
-  <a className='navbar-brand' href="#"><img className='logo' src={logo} alt="portfolio-logo..." /></a>
+  <a className='navbar-brand' href="#"><img className='logo' src={navbar} alt="portfolio-logo..." /></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <FontAwesomeIcon icon={faBars} style={{color:'#fff'}}/>
   </button>
